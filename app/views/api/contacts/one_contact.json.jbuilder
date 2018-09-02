63970613d6eb383a47a2d2971cb69b6609bc4862
kchoo2 <choo.keith@gmail.com>
2018-09-02 03:00:45 -0500

    update commit

--- app/views/api/contacts/one_contact.json.jbuilder
+++
@@ -1,5 +0,0 @@
-json.id @contacts.id
-json.first_name @contacts.first_name
-json.last_name @contacts.last_name
-json.email @contacts.email
-json.phone_number @contacts.phone_number
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
 
   namespace :api do
-    get "/one_contact_url" => 'contacts#one_contact_action'
-    get "/all_contacts_url" => 'contacts#all_contacts_action'
+    get "/contacts" => 'contacts#index'
+    get "/contacts/:id" => 'contacts#show'
+    post "/contacts" => 'contacts#create'
+    patch "/contacts/:id" => 'contacts#update'
+    delete "/contacts/:id" => 'contacts#destroy'
   end
 
 end
